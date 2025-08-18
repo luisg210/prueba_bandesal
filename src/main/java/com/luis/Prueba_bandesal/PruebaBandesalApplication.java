cc5b4e5c6bbbce375384d40214ff18240a9ab2f0
luisg210 <luisger23000@gmail.com>
2025-08-18 16:21:47 -0600

    first commit

---
+++ src/main/java/com/luis/Prueba_bandesal/PruebaBandesalApplication.java
@@ -0,0 +1,13 @@
+package com.luis.Prueba_bandesal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBandesalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBandesalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/Prueba_bandesal/PruebaBandesalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.Prueba_bandesal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBandesalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
